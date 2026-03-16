db246a6ab09c5aa0ffbbd1d575452c10c3ba79b1
katagaki <68231035+katagaki@users.noreply.github.com>
2026-03-16 19:55:35 +0900

    Initial Commit

---
+++ Reeled/App.swift
@@ -0,0 +1,17 @@
+//
+//  ReeledApp.swift
+//  Reeled
+//
+//  Created by シン・ジャスティン on 2026/03/16.
+//
+
+import SwiftUI
+
+@main
+struct ReeledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
